53fa3f6d9b32e3b549b6f782124b4585b9866112
Nicholas Castro <nicholascastro97@gmail.com>
2023-01-24 18:34:57 -0500

    text turns green when letter is correct

--- index.js
+++ index.js
@@ -1,16 +1,261 @@
-let wordBank = [
-    "array", 
-    "const", 
-    "spans", 
-    "links", 
-    "label", 
-    "input", 
-    "value", 
-    "style", 
-    "fonts", 
-    "pixel",
-    "async",
-];
+let squares = document.querySelectorAll(".square");
+
+let next1 = -1; 
+let next2 = 0;
+let next3 = 0;
+let next4 = 0;
+let next5 = 0;
+let next6 = 0;
+
+let word1 = "ARRAY";
+let word2 = "VALUE";
+let word3 = "INPUT";
+let word4 = "PIXEL";
+let word5 = "STYLE";
+let word6 = "ASYNC";
+
+let emptyArray = [[], [], [], [], [], []];
+
+document.addEventListener("keydown", function (e) {
+
+    if (e.key === "Backspace"){
+        squares[next1].innerHTML = "";
+        squares[next1].style.background = "none";
+        next1--;
+    } 
+
+    else {
+
+        next1++;
+
+        if (next1 >= 0 && next1 <= 4){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[0].push(squares[next1].innerHTML);
+            
+            let winWord = word1.split("");
+    
+            if (winWord[next1] === squares[next1].innerHTML){
+
+                squares[next1].style.background = "green";
+
+                if (next1 <= 4 && emptyArray[0].join("") === word1){
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+        }
+        
+        if (next1 >= 5 && next1 <= 9){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[1].push(squares[next1].innerHTML);
+            
+            let winWord2 = word2.split("");
+    
+            if (winWord2[next2] === squares[next1].innerHTML){
+                squares[next1].style.background = "green";
+
+                if (next2 <= 4 && emptyArray[1].join("") === word2){ 
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+
+            next2++;
+
+        }
+
+        if (next1 >= 10 && next1 <= 14){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[2].push(squares[next1].innerHTML);
+            
+            let winWord3 = word3.split("");
+    
+            if (winWord3[next3] === squares[next1].innerHTML){
+                squares[next1].style.background = "green";
+
+                if (next3 <= 4 && emptyArray[2].join("") === word3){ 
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+
+            next3++;
+
+        }
+
+        if (next1 >= 15 && next1 <= 19){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[3].push(squares[next1].innerHTML);
+            
+            let winWord4 = word4.split("");
+    
+            if (winWord4[next4] === squares[next1].innerHTML){
+                squares[next1].style.background = "green";
+
+                if (next4 <= 4 && emptyArray[3].join("") === word4){ 
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+
+            next4++;
+
+        }
+
+        if (next1 >= 20 && next1 <= 24){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[4].push(squares[next1].innerHTML);
+            
+            let winWord5 = word5.split("");
+    
+            if (winWord5[next5] === squares[next1].innerHTML){
+                squares[next1].style.background = "green";
+
+                if (next5 <= 4 && emptyArray[4].join("") === word5){ 
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+
+            next5++;
+
+        }
+
+        if (next1 >= 25 && next1 <= 29){
+
+            squares[next1].innerHTML = e.key.toUpperCase();
+
+            emptyArray[5].push(squares[next1].innerHTML);
+            
+            let winWord6 = word6.split("");
+    
+            if (winWord6[next6] === squares[next1].innerHTML){
+                squares[next1].style.background = "green";
+
+                if (next6 <= 4 && emptyArray[5].join("") === word6){ 
+                    alert("yoyo")
+                }
+
+            } else {
+                squares[next1].style.background = "red";
+            }
+
+
+            next6++;
+
+        }
+
+        
+    }
+});
+
+
+
+
+
+// let wordBank = [
+//      "array", 
+//      "const", 
+//      "spans", 
+//      "links", 
+//      "label", 
+//      "input", 
+//      "value", 
+//      "style", 
+//      "fonts", 
+//      "pixel",
+//      "async",
+// ];
+
+// // Types inside grid-blocks //
+
+// let row1 = document.querySelectorAll(".row1-block-1");
+// let row2 = document.querySelectorAll(".row2-block-1");
+// let row3 = document.querySelectorAll(".row3-block-1");
+// let row4 = document.querySelectorAll(".row4-block-1");
+// let row5 = document.querySelectorAll(".row5-block-1");
+// let row6 = document.querySelectorAll(".row6-block-1");
+
+
+// let next1 = 0;
+// let next2 = 0;
+// let next3 = 0;
+// let next4 = 0;
+// let next5 = 0;
+// let next6 = 0;
+
+
+// document.addEventListener("keydown", function (e) {
+
+//     if(e.key === "Backspace"){
+//         row1[next1].innerHTML = " ";
+//         next1--;
+//     }
+
+//     if (next1 < row1.length){
+
+//         row1[next1].innerHTML = e.key.toUpperCase();
+
+//         next1++;
+
+
+//     } else if (next1 > next2){
+
+//         row2[next2].innerHTML = e.key.toUpperCase();
+
+//         next2++;
+
+
+//     } else if (next2 > next3){
+
+//         row3[next3].innerHTML = e.key.toUpperCase();
+
+//         next3++;
+
+//     } else if (next3 > next4){
+
+//         row4[next4].innerHTML = e.key.toUpperCase();
+
+//         next4++;
+
+//     } else if (next4 > next5){
+
+//         row5[next5].innerHTML = e.key.toUpperCase();
+
+//         next5++;
+
+//     } else if (next5 > next6){
+
+//         row6[next6].innerHTML = e.key.toUpperCase();
+
+//         next6++;
+//     } 
+// });
 
 
 // Row 1: Letters
@@ -129,65 +374,3 @@ let wordBank = [
 // let letter5Row6= l5R6.innerHTML = "";
 
 // let allLettersRow6 = letter1Row6 + letter2Row6 + letter3Row6 + letter4Row6 + letter5Row6;
-
-
-
-// Types inside grid-blocks //
-
-let row1 = document.querySelectorAll(".row1-block-1");
-let row2 = document.querySelectorAll(".row2-block-1");
-let row3 = document.querySelectorAll(".row3-block-1");
-let row4 = document.querySelectorAll(".row4-block-1");
-let row5 = document.querySelectorAll(".row5-block-1");
-let row6 = document.querySelectorAll(".row6-block-1");
-
-
-let next = 0;
-let next2 = 0;
-let next3 = 0;
-let next4 = 0;
-let next5 = 0;
-let next6 = 0;
-
-
-document.addEventListener("keydown", function (e) {
-
-    if (next < row1.length){
-
-        row1[next].innerHTML = e.key.toUpperCase();
-
-        next++;
-
-    } else if (next > next2){
-
-        row2[next2].innerHTML = e.key.toUpperCase();
-
-        next2++;
-
-    } else if (next2 > next3){
-
-        row3[next3].innerHTML = e.key.toUpperCase();
-
-        next3++;
-
-    } else if (next3 > next4){
-
-        row4[next4].innerHTML = e.key.toUpperCase();
-
-        next4++;
-
-    } else if (next4 > next5){
-
-        row5[next5].innerHTML = e.key.toUpperCase();
-
-        next5++;
-
-    } else if (next5 > next6){
-
-        row6[next6].innerHTML = e.key.toUpperCase();
-
-        next6++;
-    }
-});
-
-
